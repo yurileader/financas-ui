254d275bf519261150643b209fcd50206691a4e5
Yuri Oliveira <yurileader@gmail.com>
2021-09-01 20:48:21 -0300

    Adicionando footer no projeto

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   imports: [CommonModule, RouterModule],
-  declarations: [ NavbarComponent, MenuComponent],
-  exports:[NavbarComponent, MenuComponent]
+  declarations: [ NavbarComponent, MenuComponent, FooterComponent],
+  exports:[NavbarComponent, MenuComponent, FooterComponent]
 })
 export class HomeModule {}
