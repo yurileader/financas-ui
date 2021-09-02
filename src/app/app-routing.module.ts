c8e35623bd0d8dc5f35a8f72c32d8c003dc0dba0
Yuri Oliveira <yurileader@gmail.com>
2021-09-02 13:38:49 -0300

    Adicionando Rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('./componentes/lancamento/lancamento.module').then(
+        (m) => m.LancamentoModule
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
