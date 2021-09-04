658c212864112f13a4d7444d4c1326c909c76b3e
Yuri Oliveira <yurileader@gmail.com>
2021-09-04 12:54:29 -0300

    Adicionando Componente de Lançamento
    Adicionando Componente de Lançamento com tabela

---
+++ src/app/componentes/lancamento/lancamento/lancamento.component.ts
@@ -0,0 +1,27 @@
+import { __values } from 'tslib';
+import { LancamentosService } from './../../../core/services/lancamentos.service';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Lancamentos } from './../../../core/models/lancamento';
+
+@Component({
+  selector: 'app-lancamento',
+  templateUrl: './lancamento.component.html',
+  styleUrls: ['./lancamento.component.css'],
+})
+export class LancamentoComponent implements OnInit {
+  lancamentos$?: Observable<any>;
+  lancamentos!: Lancamentos;
+
+  constructor(private lancamentosService: LancamentosService) {}
+
+  ngOnInit(): void {
+    this.lancamentosService.listaLancamento().subscribe(
+      (lancamento) => {
+        this.lancamentos = lancamento.content;
+        console.log(this.lancamentos);
+      },
+      (error) => console.log('error', error)
+    );
+  }
+}
