7d069f6ab556b539ae787585b9243625ff3d45b6
yurileader <yurileader@gmail.com>
2021-09-08 18:01:09 -0300

    Fix rotas de lançamentos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: '', pathMatch: 'full', redirectTo: 'lancamentos' },
   {
-    path: 'home',
+    path: 'lancamentos',
     loadChildren: () =>
       import('./componentes/lancamento/lancamento.module')
       .then((m) => m.LancamentoModule
