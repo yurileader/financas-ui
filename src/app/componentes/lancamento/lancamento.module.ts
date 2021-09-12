79ffacedbc7efd6ccebced36241617187f7de38f
yurileader <yurileader@gmail.com>
2021-09-11 21:52:01 -0300

    Adicionando ReactiveForm em lançamento

--- src/app/componentes/lancamento/lancamento.module.ts
+++ src/app/componentes/lancamento/lancamento.module.ts
@@ -3,20 +3,23 @@ import { NgModule, LOCALE_ID } from '@angular/core';
 import { CommonModule, registerLocaleData } from '@angular/common';
 import localePt from '@angular/common/locales/pt';
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { LancamentoComponent } from './lancamento/lancamento.component';
 import { LancamentosRoutes } from './lancamentos-routing.module';
+import { LancamentoFormComponent } from './lancamento-form/lancamento-form.component';
 
 import { TableModule } from 'primeng/table';
 import { CardModule } from 'primeng/card';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { TooltipModule } from 'primeng/tooltip';
+import { SelectButtonModule } from 'primeng/selectbutton';
 
 registerLocaleData(localePt, 'pt-BR');
 
 @NgModule({
-  declarations: [LancamentoComponent],
+  declarations: [LancamentoComponent, LancamentoFormComponent],
   imports: [
     CommonModule,
     RouterModule,
@@ -26,6 +29,9 @@ registerLocaleData(localePt, 'pt-BR');
     InputTextModule,
     ButtonModule,
     TooltipModule,
+    ReactiveFormsModule,
+    SelectButtonModule,
+
   ],
   exports: [LancamentoComponent],
   providers: [LancamentosService, { provide: LOCALE_ID, useValue: 'pt-BR' }],
