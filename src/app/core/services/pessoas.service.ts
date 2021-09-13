73028356d9db9db9dce9a6c5c83e69454aabc52a
yurileader <yurileader@gmail.com>
2021-09-13 20:03:02 -0300

    Adicionando metodo para cadastrar pessoa na api

--- src/app/core/services/pessoas.service.ts
+++ src/app/core/services/pessoas.service.ts
@@ -1,6 +1,9 @@
-import { environment } from './../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+import { environment } from './../../../environments/environment';
+
+import { Pessoa } from '../models/pessoa';
 
 const API = environment.apiURL;
 
@@ -13,5 +16,9 @@ constructor(private http: HttpClient) { }
 
   listarPessoas(){
     return this.http.get<any>(`${API}/pessoas`);
+  }
+
+  cadastrar(pessoa:Pessoa){
+    return this.http.post<any>(`${API}/pessoas`, pessoa)
   }
 }
