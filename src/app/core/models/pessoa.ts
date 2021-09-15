ff59ccd1ba74fd50b07fbf330fcd80211b4383e4
yurileader <yurileader@gmail.com>
2021-09-14 21:18:59 -0300

    Removendo interface Endereco de pessoa

--- src/app/core/models/pessoa.ts
+++ src/app/core/models/pessoa.ts
@@ -1,17 +1,9 @@
+import { Endereco } from "./endereco";
+
 export interface Pessoa {
   id:number;
   nome: string;
   endereco: Endereco
-}
-
-export interface Endereco {
-  logradouro: string;
-  numero: string;
-  complemento: string;
-  bairro: string;
-  cep: string;
-  cidade: string;
-  estado: string;
 }
 
 export type Pessoas = Array<Pessoa>;
